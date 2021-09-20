0fbe5723266dc985f5da6c8cb654b651e3ba9ebb
Kateryna Ueno <katerynaueno@gmail.com>
2021-09-20 14:43:11 +0100

    created model and controller

---
+++ app/models/challenge1.rb
@@ -0,0 +1,2 @@
+class Challenge1 < ApplicationRecord
+end
---
+++ db/migrate/20210920133926_create_challenge1s.rb
@@ -0,0 +1,8 @@
+class CreateChallenge1s < ActiveRecord::Migration[6.0]
+  def change
+    create_table :challenge1s do |t|
+
+      t.timestamps
+    end
+  end
+end
